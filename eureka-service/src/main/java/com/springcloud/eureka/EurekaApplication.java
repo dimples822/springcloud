7eed33fb7bf7b300d49aa002a399b92e4173240e
dimples822 <1126834403@qq.com>
2019-08-12 22:01:35 +0800

    新增springcloud注册中心

---
+++ eureka-service/src/main/java/com/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,54 @@
+package com.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * SpringCloud注册中心
+ *
+ * @author zhongyj
+ * @date 2019/8/12
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
