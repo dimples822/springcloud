ac6ecb4b7e4ed58c3691e45f2e46f11eb51e86f8
dimples822 <1126834403@qq.com>
2019-08-13 21:24:12 +0800

    完成eureka客户端的测试

--- eureka-client/src/main/java/com/springcloud/eureka/client/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/springcloud/eureka/client/EurekaClientApplication.java
@@ -2,8 +2,16 @@ package com.springcloud.eureka.client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * eureka客户端测试
+ *
+ * @author zhongyj
+ * @date 2019/8/13
+ */
 @SpringBootApplication
+@EnableEurekaClient
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
