428acae865241f9f66f75f7018fc1ea61b038825
dimples822 <1126834403@qq.com>
2019-08-13 22:07:33 +0800

    添加Zuul网关模块

--- eureka-service/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
+++ eureka-service/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * SpringCloud注册中心
  *
@@ -12,10 +14,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
+@Slf4j
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+        log.info("*****************Eureka Server 程序已启动******************");
     }
 
 }
--- zuul-gateway/src/main/java/com/springcloud/zuul/ZuulGatewayApplication.java
+++ zuul-gateway/src/main/java/com/springcloud/zuul/ZuulGatewayApplication.java
@@ -1,25 +1,27 @@
-package com.springcloud.eureka.client;
+package com.springcloud.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * eureka客户端测试
+ * Zuul网关示例
  *
  * @author zhongyj
  * @date 2019/8/13
  */
 @SpringBootApplication
+@EnableZuulServer
 @EnableEurekaClient
 @Slf4j
-public class EurekaClientApplication {
+public class ZuulGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaClientApplication.class, args);
-        log.info("*****************Eureka Client 程序已启动******************");
+        SpringApplication.run(ZuulGatewayApplication.class, args);
+        log.info("*****************Zuul 程序已启动******************");
     }
 
 }
