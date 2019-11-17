251f63e7b3a3d6da2b3aa9a815926f6be981dd67
dimples <1126834403@qq.com>
2019-11-17 15:35:23 +0800

    eureka server添加docker打包

--- eureka-server/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/springcloud/eureka/EurekaServerApplication.java
@@ -12,9 +12,10 @@ import lombok.extern.slf4j.Slf4j;
  * @author zhongyj
  * @date 2019/8/12
  */
+@Slf4j
 @SpringBootApplication
 @EnableEurekaServer
-@Slf4j
+
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
