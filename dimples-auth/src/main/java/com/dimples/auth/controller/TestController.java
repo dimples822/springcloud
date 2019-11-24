d2cd996dae5fd047052a655f890956558ea27af2
dimples <1126834403@qq.com>
2019-11-24 12:47:19 +0800

    删除Nacos的配置中心

--- dimples-auth/src/main/java/com/dimples/auth/controller/TestController.java
+++ dimples-auth/src/main/java/com/dimples/auth/controller/TestController.java
@@ -3,6 +3,8 @@ package com.dimples.auth.controller;
 import com.dimples.common.annotation.OpsLog;
 import com.dimples.common.eunm.OpsLogTypeEnum;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,13 +22,19 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RestController
 @Slf4j
+@RefreshScope
 public class TestController {
+
+    @Value("${dimples:test:Nacos配置中心测试}")
+    private String test;
 
     @OpsLog(value = "多模块引用测试", type = OpsLogTypeEnum.TEST)
     @GetMapping("/{name}")
     public Map<String, Object> testZuul(@PathVariable String name) {
         Map<String, Object> map = new HashMap<>(1);
         map.put("name", name);
+        map.put("nacos-test", test);
+        log.info(test);
         return map;
     }
 }
