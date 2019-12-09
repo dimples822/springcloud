cb404fc48e2b9822d09a9668f82eb00d903d0cce
dimples <1126834403@qq.com>
2019-12-10 00:00:36 +0800

    添加redis的配置类

--- dimples-auth/src/main/java/com/dimples/auth/config/Auth2AuthorizationServerConfig.java
+++ dimples-auth/src/main/java/com/dimples/auth/config/Auth2AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.dimples.auth.config;
 
 import com.dimples.auth.properties.AuthProperties;
+import com.dimples.auth.service.impl.RedisClientDetailsServiceImpl;
 import com.dimples.auth.service.impl.UserDetailsServiceImpl;
 import com.dimples.auth.translator.AuthWebResponseExceptionTranslator;
 
@@ -8,17 +9,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.password.ResourceOwnerPasswordTokenGranter;
+import org.springframework.security.oauth2.provider.request.DefaultOAuth2RequestFactory;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.DefaultUserAuthenticationConverter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import java.util.UUID;
 
 import javax.annotation.Resource;
 
@@ -40,6 +49,10 @@ public class Auth2AuthorizationServerConfig extends AuthorizationServerConfigure
     private UserDetailsServiceImpl userDetailsService;
     @Resource
     private AuthWebResponseExceptionTranslator exceptionTranslator;
+    @Resource
+    private RedisConnectionFactory redisConnectionFactory;
+    @Resource
+    private RedisClientDetailsServiceImpl redisClientDetailsService;
 
     /**
      * 开启授权服务器的access_token以及check_token节点
@@ -94,7 +107,8 @@ public class Auth2AuthorizationServerConfig extends AuthorizationServerConfigure
                 // 配置access_token使用的转换
                 .accessTokenConverter(jwtAccessTokenConverter())
                 // 配置异常翻译
-                .exceptionTranslator(exceptionTranslator);
+                .exceptionTranslator(exceptionTranslator)
+                .userDetailsService(userDetailsService);
     }
 
     /**
@@ -106,7 +120,11 @@ public class Auth2AuthorizationServerConfig extends AuthorizationServerConfigure
      */
     @Bean
     public TokenStore tokenStore() {
-        return new JwtTokenStore(jwtAccessTokenConverter());
+        // 默认可配置成 return new JwtTokenStore(jwtAccessTokenConverter())
+        RedisTokenStore redisTokenStore = new RedisTokenStore(redisConnectionFactory);
+        // 解决每次生成的 token都一样的问题
+        redisTokenStore.setAuthenticationKeyGenerator(oAuth2Authentication -> UUID.randomUUID().toString());
+        return redisTokenStore;
     }
 
     /**
@@ -119,6 +137,11 @@ public class Auth2AuthorizationServerConfig extends AuthorizationServerConfigure
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("dimples.jks"), "dimples".toCharArray());
         JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        DefaultAccessTokenConverter defaultAccessTokenConverter = (DefaultAccessTokenConverter) converter.getAccessTokenConverter();
+        DefaultUserAuthenticationConverter defaultUserAuthenticationConverter = new DefaultUserAuthenticationConverter();
+        defaultUserAuthenticationConverter.setUserDetailsService(userDetailsService);
+        defaultAccessTokenConverter.setUserTokenConverter(defaultUserAuthenticationConverter);
+        // 配置私钥加密
         converter.setKeyPair(keyStoreKeyFactory.getKeyPair("dimples"));
         return converter;
     }
@@ -135,6 +158,30 @@ public class Auth2AuthorizationServerConfig extends AuthorizationServerConfigure
         tokenServices.setTokenStore(tokenStore());
         tokenServices.setSupportRefreshToken(true);
         return tokenServices;
+    }
+
+    /**
+     * 根据用户密码的方式获取token
+     *
+     * @param authenticationManager AuthenticationManager
+     * @param oAuth2RequestFactory  OAuth2RequestFactory
+     * @return ResourceOwnerPasswordTokenGranter
+     */
+    @Bean
+    public ResourceOwnerPasswordTokenGranter resourceOwnerPasswordTokenGranter(AuthenticationManager authenticationManager, OAuth2RequestFactory oAuth2RequestFactory) {
+        DefaultTokenServices defaultTokenServices = defaultTokenServices();
+        if (properties.getEnableJwt()) {
+            defaultTokenServices.setTokenEnhancer(jwtAccessTokenConverter());
+        }
+        return new ResourceOwnerPasswordTokenGranter(authenticationManager, defaultTokenServices, redisClientDetailsService, oAuth2RequestFactory);
+    }
+
+    /**
+     * @return DefaultOAuth2RequestFactory
+     */
+    @Bean
+    public DefaultOAuth2RequestFactory oAuth2RequestFactory() {
+        return new DefaultOAuth2RequestFactory(redisClientDetailsService);
     }
 
 }
---
+++ dimples-auth/src/main/java/com/dimples/auth/service/impl/RedisClientDetailsServiceImpl.java
@@ -0,0 +1,111 @@
+package com.dimples.auth.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.security.oauth2.common.exceptions.InvalidClientException;
+import org.springframework.security.oauth2.provider.ClientDetails;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhongyj <1126834403@qq.com><br/>
+ * @date 2019/12/9
+ */
+@Slf4j
+@Service
+public class RedisClientDetailsServiceImpl extends JdbcClientDetailsService {
+
+    @Resource
+    private RedisServiceImpl redisService;
+
+    /**
+     * 缓存 client的 redis key，这里是 hash结构存储
+     */
+    private static final String CACHE_CLIENT_KEY = "client_details";
+
+    public RedisClientDetailsServiceImpl(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    public ClientDetails loadClientByClientId(String clientId) throws InvalidClientException {
+        ClientDetails clientDetails;
+        String value = (String) redisService.hget(CACHE_CLIENT_KEY, clientId);
+        if (StringUtils.isBlank(value)) {
+            clientDetails = cacheAndGetClient(clientId);
+        } else {
+            clientDetails = JSONObject.parseObject(value, BaseClientDetails.class);
+        }
+
+        return clientDetails;
+    }
+
+    /**
+     * 缓存 client并返回 client
+     *
+     * @param clientId clientId
+     */
+    public ClientDetails cacheAndGetClient(String clientId) {
+        ClientDetails clientDetails;
+        clientDetails = super.loadClientByClientId(clientId);
+        if (clientDetails != null) {
+            redisService.hset(CACHE_CLIENT_KEY, clientId, JSONObject.toJSONString(clientDetails));
+        }
+        return clientDetails;
+    }
+
+    /**
+     * 删除 redis缓存
+     *
+     * @param clientId clientId
+     */
+    public void removeRedisCache(String clientId) {
+        redisService.hdel(CACHE_CLIENT_KEY, clientId);
+    }
+
+    /**
+     * 将 oauth_client_details全表刷入 redis
+     */
+    public void loadAllClientToCache() {
+        if (redisService.hasKey(CACHE_CLIENT_KEY)) {
+            return;
+        }
+        log.info("将oauth_client_details全表刷入redis");
+
+        List<ClientDetails> list = super.listClientDetails();
+        if (CollectionUtils.isEmpty(list)) {
+            log.error("oauth_client_details表数据为空，请检查");
+            return;
+        }
+        list.forEach(client -> redisService.hset(CACHE_CLIENT_KEY, client.getClientId(), JSONObject.toJSONString(client)));
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
