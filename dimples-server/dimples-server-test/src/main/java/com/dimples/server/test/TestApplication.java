d372ad3b8e0a3e79f036af18369972e36ce4a43b
dimples <1126834403@qq.com>
2019-12-30 21:38:41 +0800

    抽取security配置到nacos 增加test模块的资源服务器配置

---
+++ dimples-server/dimples-server-test/src/main/java/com/dimples/server/test/TestApplication.java
@@ -0,0 +1,43 @@
+package com.dimples.server.test;
+
+import com.dimples.common.annotation.EnableAuthExceptionHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author zhongyj <1126834403@qq.com><br/>
+ * @date 2019/12/30
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAuthExceptionHandler
+@Slf4j
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class TestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestApplication.class, args);
+        log.info("======================================= 程序启动成功 =======================================");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ dimples-server/dimples-server-test/src/main/java/com/dimples/server/test/config/TestResourceServerConfigure.java
@@ -0,0 +1,61 @@
+package com.dimples.server.test.config;
+
+import com.dimples.common.handler.AuthAccessDeniedHandler;
+import com.dimples.common.handler.AuthExceptionEntryPoint;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+/**
+ * 资源服务器配置
+ *
+ * @author zhongyj <1126834403@qq.com><br/>
+ * @date 2019/12/27
+ */
+@Configuration
+@EnableResourceServer
+public class TestResourceServerConfigure extends ResourceServerConfigurerAdapter {
+
+    private AuthAccessDeniedHandler accessDeniedHandler;
+    private AuthExceptionEntryPoint exceptionEntryPoint;
+
+    @Autowired
+    public TestResourceServerConfigure(AuthAccessDeniedHandler accessDeniedHandler, AuthExceptionEntryPoint exceptionEntryPoint) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.exceptionEntryPoint = exceptionEntryPoint;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .requestMatchers().antMatchers("/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").authenticated();
+    }
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) {
+        resources.authenticationEntryPoint(exceptionEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
